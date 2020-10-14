bbcfacc94cb2c6d7331948c685575730708f6535
Savindu Kalsara Ekanayake <47809365+savinduekanayake@users.noreply.github.com>
2020-10-15 00:50:01 +0530

    started unit testing

---
+++ src/test/java/io/javabrains/MathUtilsTest.java
@@ -0,0 +1,12 @@
+package io.javabrains;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathUtilsTest {
+    @Test
+    void test() {
+        System.out.println("This test run");
+    }
+}
