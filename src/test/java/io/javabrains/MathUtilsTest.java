3fd7ef3666f12a2e6b0d1f05185a9a99af16a2c3
Savindu Kalsara Ekanayake <47809365+savinduekanayake@users.noreply.github.com>
2020-10-15 13:24:46 +0530

    Added TestInfo and TestReporter to make logs

--- src/test/java/io/javabrains/MathUtilsTest.java
+++ src/test/java/io/javabrains/MathUtilsTest.java
@@ -11,6 +11,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class MathUtilsTest {
 
     MathUtils mathUtils;
+    TestInfo testInfo;
+    TestReporter testReporter;
 
     @BeforeAll
     static void beforeAllInit() {
@@ -18,8 +20,11 @@ class MathUtilsTest {
     }
 
     @BeforeEach
-    void init() {
+    void init(TestInfo testInfo, TestReporter testReporter) {
+        this.testInfo = testInfo;
+        this.testReporter = testReporter;
         mathUtils = new MathUtils();
+        testReporter.publishEntry("Running" + testInfo.getDisplayName() + "With tags"+ testInfo.getTags());
     }
 
     @AfterEach
@@ -29,13 +34,14 @@ class MathUtilsTest {
 
     @Nested
     @DisplayName("add method")
+    @Tag("Math")
     class AddTest {
         @Test
         @DisplayName("Testing add method for +")
         void testAddPositive() {
             int expected = 2;
             int actual = mathUtils.add(1,1);
-            assertEquals(expected, actual, "should return the right sum");
+            assertEquals(expected, actual, () -> "should return the right sum"+expected+ "But returns the"+actual);
         }
 
         @Test
@@ -48,6 +54,7 @@ class MathUtilsTest {
     }
 
     @Test
+    @Tag("Math")
 //    @EnabledOnOs(OS.LINUX)
     @DisplayName("Testing Divide method")
     void testDivide() {
@@ -56,9 +63,10 @@ class MathUtilsTest {
     }
 
     @Test
-    @DisplayName("Multiply nethod")
+    @Tag("Math")
+    @DisplayName("Multiply method")
     void testMultiply(){
-//        assertEquals(4,mathUtils.multiply(2,2),"should return the right product");
+
             assertAll(
                     () -> assertEquals(4,mathUtils.multiply(2,2)),
                     () -> assertEquals(0,mathUtils.multiply(0,2))
@@ -66,7 +74,9 @@ class MathUtilsTest {
     }
 
 
-    @Test
+
+    @Tag("Circular")
+    @RepeatedTest(3)
     void testComputeCircleRadius() {
         assertEquals(314.1592653589793,mathUtils.computeCircleArea(10),"Should return right circle area");
     }
