4ad1ae2a1184bdf173e1ad8af6e49cc26648f24b
Savindu Kalsara Ekanayake <47809365+savinduekanayake@users.noreply.github.com>
2020-10-15 11:00:22 +0530

    use new anotations and use assertAll for multiple things test in same method

--- src/test/java/io/javabrains/MathUtilsTest.java
+++ src/test/java/io/javabrains/MathUtilsTest.java
@@ -1,6 +1,9 @@
 package io.javabrains;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.api.Assertions.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,6 +27,7 @@ class MathUtilsTest {
     }
 
     @Test
+    @DisplayName("Testing add method")
     void testAdd() {
         int expected = 2;
         int actual = mathUtils.add(1,1);
@@ -31,13 +35,33 @@ class MathUtilsTest {
     }
 
     @Test
+//    @EnabledOnOs(OS.LINUX)
+    @DisplayName("Testing Divide method")
     void testDivide() {
         assertThrows(ArithmeticException.class, () -> mathUtils.divide(1,0),"Divide by zero should throw");
 
     }
 
     @Test
+    @DisplayName("Multiply nethod")
+    void testMultiply(){
+//        assertEquals(4,mathUtils.multiply(2,2),"should return the right product");
+            assertAll(
+                    () -> assertEquals(4,mathUtils.multiply(2,2)),
+                    () -> assertEquals(0,mathUtils.multiply(0,2))
+            );
+    }
+
+
+    @Test
     void testComputeCircleRadius() {
         assertEquals(314.1592653589793,mathUtils.computeCircleArea(10),"Should return right circle area");
+    }
+
+    @Test
+    @Disabled
+    @DisplayName("Test Disable")
+    void testDisabled(){
+        fail("The test should be disabled");
     }
 }
